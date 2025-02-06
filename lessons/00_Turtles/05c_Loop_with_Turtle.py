03a701036aa09a4cc19a3f1f91d487a944aef01f
MinecraftMcGee <vstarr1@mac.com>
2025-02-06 00:42:41 +0000

    I created a 4 by 4 grid of squares with 125 lines of code

--- lessons/00_Turtles/05c_Loop_with_Turtle.py
+++ lessons/00_Turtles/05c_Loop_with_Turtle.py
@@ -1,36 +1,128 @@
+import turtle
+turtle.setup(width=550, height=625)
+
 tina = turtle.Turtle()
 tina.shape('triangle')
 tina.speed(0)
 
+def square_grid():
+    def square(left_right):
+        if left_right:
+            for i in range(4):
+                tina.forward(100)
+                tina.left(90)
+        else:
+            for i in range(4):
+                tina.forward(100)
+                tina.right(90)
 
-tina.pendown()
+    tina.pendown()
 
-for i in range(4):
-    tina.forward(100)
+    square(True)
+
+    tina.penup()
+    tina.forward(150)
+    tina.pendown()
+
+    square(True)
+
+    tina.penup()
+    tina.right(180)
+    tina.forward(200)
+
+    tina.pendown()
+
+    square(False)
+
+    tina.penup()
     tina.left(90)
+    tina.forward(50)
 
-tina.right(135)
+    tina.pendown()
 
-for i in range(4):
-    tina.forward(100)
+    square(False)
+
     tina.left(90)
+    tina.penup()
+    tina.forward(50)
+    tina.pendown()
 
-tina.right(35)
+    square(False)
 
-for i in range(4):
-    tina.forward(100)
+    tina.penup()
+    tina.forward(150)
+    tina.pendown()
+
+    square(False)
+
+    tina.left(90)
+    tina.penup()
+    tina.forward(200)
+    tina.pendown()
+
+    square(False)
+
+    tina.left(90)
+    tina.penup()
+    tina.forward(50)
+    tina.pendown()
+
+    square(False)
+
+    tina.penup()
+    tina.forward(150)
+    tina.pendown()
+
+    square(False)
+
+    tina.penup()
+    tina.forward(250)
     tina.right(90)
+    tina.pendown()
 
+    square(False)
 
+    tina.penup()
+    tina.right(180)
+    tina.forward(50)
+    tina.pendown()
 
+    square(True)
+    tina.forward(100)
 
+    tina.penup()
+    tina.forward(50)
+    tina.pendown()
 
+    square(True)
 
+    tina.penup()
+    tina.forward(150)
+    tina.pendown()
 
+    square(True)
 
+    tina.penup()
+    tina.left(90)
+    tina.forward(250)
+    tina.right(90)
+    tina.pendown()
 
+    square(False)
 
+    tina.penup()
+    tina.left(90)
+    tina.forward(50)
+    tina.pendown()
 
+    square(False)
 
+    tina.penup()
+    tina.forward(150)
+    tina.pendown()
+
+    square(False)
+
+square_grid()
 
 turtle.exitonclick()
